900b3b942ad554c521ba47fb839cefe4be0d6a6d
Nkaka23dev <nkakaeric96@gmail.com>
2023-03-27 06:52:37 +0200

    feat: intro and underthe wood of node

---
+++ app.js
@@ -0,0 +1,13 @@
+// console.log(global)
+
+// global.setTimeout(() => {
+//    console.log("Hello There,") 
+// }, 2000);
+
+// const int = setInterval(() => {
+//     console.log("Inside intervals")
+// }, 1000); 
+
+console.log(__dirname);
+console.log(__filename)
+
