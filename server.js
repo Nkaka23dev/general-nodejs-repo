7504957ff9e2f450114fdf6f1a9280cd9a6d9cd0
Nkaka23dev <nkakaeric96@gmail.com>
2023-04-03 10:08:07 +0200

    feat: finished express

--- server.js
+++ server.js
@@ -1,41 +1,40 @@
-const http = require("http");
-const fs = require("fs")
-const _ = require("lodash")
+const http = require('http');
+const fs = require('fs');
+
 
 const server = http.createServer((req, res) => {
-    console.log(_.random(0, 100))
-    res.setHeader('Content-Type', 'text/html');
-    let path = "./views/";
+  res.setHeader('Content-Type', 'text/html');
 
-    switch (req.url) {
-        case '/':
-            path += 'index.html';
-            res.statusCode = 200
-            break;
-        case '/about':
-            path += 'about.html';
-            res.statusCode = 200
-            break;
-        case '/about-us':
-            res.statusCode = 301;
-            res.setHeader('Location', '/about');
-            res.end();
-            break;
-        default:
-            path += '404.html'
-            res.statusCode = 404
-            break
+  let path = './views/';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break
+    case '/about-us':
+      res.setHeader('Location', '/about');
+      res.statusCode = 301;
+      res.end();
+      break;
+    default:
+      path += '404.html'
+      res.statusCode = 200;
+      break;
+  }
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log("Error occured!")
+    } else {
+      res.write(data)
+      res.end()
     }
-    fs.readFile(path, (err, data) => {
-        if (err) {
-            console.log("Error occured", err);
-            res.end();
-        } else {
-            res.write(data);
-            res.end()
-        }
-    })
+  })
 })
+
 server.listen(3000, 'localhost', () => {
-    console.log("We are listening on port 3000")
-});
+  console.log("Listening on port 3000")
+})
