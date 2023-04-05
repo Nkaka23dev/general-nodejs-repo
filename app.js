f77e3d9b92b4f255c51daa34dc3ecad9d0e2caae
Nkaka23dev <nkakaeric96@gmail.com>
2023-04-05 07:09:05 +0200

    feat: finish static files

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
 
 app.listen(8000)
 
+app.use(morgan('dev'))
+// ejs setting 
+
+app.use(express.static('public'))
+
+app.set("view engine", "ejs")
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname })
+    const blogs = [
+        { title: 'Yoshi Find Eggs', snippet: "Lorem  sit amet consectetur adipisicing elit." },
+        { title: 'Yoshi Find Eggs', snippet: "Lorem  sit amet consectetur adipisicing elit." },
+        { title: 'Yoshi Find Eggs', snippet: "Lorem  sit amet consectetur adipisicing elit." }
+    ]
+    res.render('index', { title: "Home", blogs })
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about', { title: "About" })
 })
-
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: "Create" })
 })
-
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404', { title: "404" })
 })
