fcd7c0978ce7707eb1b82cfd3c53182dbc0ad9a8
Nkaka23dev <nkakaeric96@gmail.com>
2023-05-11 12:44:51 +0300

    feat: post,delet and patch in progress

--- app.js
+++ app.js
@@ -17,30 +17,6 @@ app.use(express.static('public'))
 // In case the ejs files are not in the folder called view you can set it.
 // app.set('views', 'myviews');
 
-app.use('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: "Onother blog is writen",
-        snippet: "hello There new blog",
-        body: "Blogs"
-    })
-    blog.save()
-        .then((result) => {
-            res.send(result);
-        }).catch(err => {
-            console.log(err)
-        })
-});
-app.use('/get-all-blogs', (req, res) => {
-    Blog.find()
-        .then(response => {
-            res.send(response)
-        }).catch(err => console.log(err))
-})
-app.use('/single-blog', (req, res) => {
-    Blog.findById("645c1116f54ff81523563910").then(response => {
-        res.send(response)
-    }).catch(err => { console.log(err) })
-})
 app.use((req, res, next) => {
     console.log('Host Name', req.hostname);
     console.log('Method', req.method);
@@ -49,15 +25,19 @@ app.use((req, res, next) => {
 })
 app.use((req, res, next) => {
     console.log("In the next middleware")
-    next();
+    next(); 
 })
 
 app.get('/', (req, res) => {
-    const blogs = [
-        { title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet consectetur" },
-        { title: "mario finds stars", snippet: "Lorem ipsum dolor sit amet consectetur" },
-        { title: "HOW to defeat bowser", snippet: "Lorem ipsum dolor sit amet consectetur" }]
-    res.render('index', { title: "Home", blogs: blogs })
+    res.redirect("/blogs")
+})
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 }).then(result => {
+        res.render('index', { title: "All blogs", blogs: result })
+    }).catch(err => {
+        console.log(err)
+    })
 })
 app.get('/about', (req, res) => {
     res.render('about', { title: "About" })
