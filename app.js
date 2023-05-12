a4ed9df4e7dee36b53e140c82886a91f98cc0941
Nkaka23dev <nkakaeric96@gmail.com>
2023-05-12 16:11:10 +0300

    feat: done with delet functionality

--- app.js
+++ app.js
@@ -6,18 +6,46 @@ const app = express();
 const dbURL = 'mongodb+srv://nkakaeric:isheja1994@cluster0.uwu4q0a.mongodb.net/';
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 mongoose.connect(dbURL).then((result) => {
     app.listen(8000)
 }).catch(err => {
     console.log("Got an error", err)
 })
-app.use(express.static('public'))
-
-app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.redirect("/blogs")
+})
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save().then(results => {
+        res.redirect('/blogs')
+    }).catch(err => {
+        console.log(err)
+    })
+});
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a blog' })
+})
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id).then(result => {
+        res.json({ redirect: '/blogs' })
+    }
+    ).catch(err => {
+        console.log(err)
+    })
+})
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id).then(result => {
+        res.render('blogDetails', { blog: result, title: "Blog Details" })
+    }
+    ).catch(err => {
+        console.log(err)
+    })
 })
 
 app.get('/blogs', (req, res) => {
@@ -27,20 +55,14 @@ app.get('/blogs', (req, res) => {
         console.log(err)
     })
 })
-app.post('/blogs', (req, res) => {
-  console.log(req.body)
-});
 app.get('/about', (req, res) => {
     res.render('about', { title: "About" })
 })
 app.get('/about-us', (req, res) => {
     res.redirect('/about')
 })
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a blog' })
-})
 app.use((req, res) => {
     res.render('404', { title: "404 not found" })
-});   
+});
 
 
