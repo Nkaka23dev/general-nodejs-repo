fa7d9ed3401aa74a2db961c9b42c8f5ece582cd0
ericnkaka <nkakaeric96@gmail.com>
2023-11-28 21:49:03 +0200

    feat: done

--- modules.js
+++
@@ -1,6 +0,0 @@
-const { ages, people } = require("./people");
-const os = require('os')
-// console.log(ages)
-// console.log(people)
-
-console.log(os.platform)
--- people.js
+++
@@ -1,9 +0,0 @@
-const people = ["Nkaka", "Edson","Victor", "Aldo"];
-const ages = [34,5,645,345]
-
-
-;
-module.exports = {
-    people, 
-    ages
-}
