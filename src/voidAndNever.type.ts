c53f67e89d6ee43066247f1e75f51a41b02a8eac
ericnkaka <nkakaeric96@gmail.com>
2023-11-29 16:57:33 +0200

    done:

---
+++ src/voidAndNever.type.ts
@@ -0,0 +1,7 @@
+const value: void = undefined;
+// const value2: never = undefined;
+
+//void means a funnction will not return a value or it will return underfined
+//never means a funnction will never returnse example functions that throw errors, or infinity loop
+
+//Also you can not assign a value to a type of never but for void you can assign underfined
