aabc12464d0d3d2a51a89bbfd68ff3c5189473cc
ericnkaka <nkakaeric96@gmail.com>
2024-03-24 21:12:10 +0200

    feat: starting service workers

--- src2/jaggedArray.js
+++
@@ -1,9 +0,0 @@
-function createJaggedArray(rows, columns) {
-  const jaggedArray = new Array(rows);
-  for (let i = 0; i < rows; i++) {
-    jaggedArray[i] = new Array(columns);
-  }
-  return jaggedArray;
-}
-
-console.log(createJaggedArray(3, 3));
